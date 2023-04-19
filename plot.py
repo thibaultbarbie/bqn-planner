2149120315f3f2e569c8509c490cbb8f4a600a48
Thibault <thibault.barbie@free.fr>
2023-04-19 22:29:12 +0900

    Updated visualizer

--- plot.py
+++ plot.py
@@ -13,12 +13,15 @@ with open('solution.csv', newline='') as csvfile:
 
 data_numpy = np.array(data)
 print(data_numpy)
-plt.plot(data_numpy[:,0], data_numpy[:,1])
+plt.plot(data_numpy[:,0], data_numpy[:,1], color = "green")
 
 
-circle = plt.Circle((0.5, 0.5), 0.173, color="r")
+circle = plt.Circle((0.5, 0.5), 0.173, color=np.array([97, 76, 117])/255)
 ax = plt.gca()
 
 ax.add_patch(circle)
+plt.xlim([0, 1])
+plt.ylim([0, 1])
 
+plt.gca().set_aspect('equal')
 plt.show()
